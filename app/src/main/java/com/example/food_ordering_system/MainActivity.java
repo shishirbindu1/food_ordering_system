3d6b35ccb142469967695b2bd21ec0feb20a7f3b
shishir <bindushishir004@gmail.com>
2023-03-01 17:34:44 +0545

    onclick event listener is added

--- app/src/main/java/com/example/food_ordering_system/MainActivity.java
+++ app/src/main/java/com/example/food_ordering_system/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.food_ordering_system;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button pizzaButton, burgerButton, pastryButton;
+
+    //Declaring a string to take my choice in it
+
+    String choices = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pizzaButton = (Button) findViewById(R.id.pizzaButton);
+        burgerButton = (Button) findViewById(R.id.burgerButton);
+        pastryButton = (Button) findViewById(R.id.pastryButton);
     }
 }
