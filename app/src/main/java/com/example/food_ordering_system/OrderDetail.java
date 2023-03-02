424d5753e8e490c005d8199f71361ff081e84633
shishir <bindushishir004@gmail.com>
2023-03-02 09:00:22 +0545

    linking order detail page with main activity page and backend part is completed

--- app/src/main/java/com/example/food_ordering_system/OrderDetail.java
+++ app/src/main/java/com/example/food_ordering_system/OrderDetail.java
@@ -3,12 +3,32 @@ package com.example.food_ordering_system;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class OrderDetail extends AppCompatActivity {
+
+        TextView listView, priceView;
+        String list_choice;
+        Double price_bd, price_usd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_detail);
+        listView = (TextView) findViewById(R.id.listView);
+        priceView = (TextView) findViewById((R.id.priceView));
+
+        //getting the passed value from mainActivity
+
+        Bundle bundle =getIntent().getExtras();
+        list_choice = bundle.getString("choices");
+        price_bd = bundle.getDouble("price");
+
+        //converting price in usd
+        listView.setText(list_choice);
+        priceView.setText(price_bd.toString());
+
+
+
     }
 }
